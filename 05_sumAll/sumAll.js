1782bdbf240402fcf90e0de236f8fa2dc7373d11
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-07-28 20:42:32 +0200

    Create sumAll function
    Created a function that takes in two integers and
    sums upp all the values between them (and the
    passed values themselves). Also passed all 7 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
 
+    let min = null;
+    let max = null;
+    let sum = 0;
+    let error = "ERROR"
+
+    if(number1 < 0 || number2 < 0){
+        return error;
+    } else if (!Number.isInteger(number1) || !Number.isInteger(number2)){
+        return error;
+    }
+
+    if(number1 > number2){
+        max = number1;
+        min = number2;
+    }else{
+        max = number2;
+        min = number1;
+    }
+
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
