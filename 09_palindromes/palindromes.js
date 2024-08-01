18207875feeda2b530eb663591ddffb732e6a127
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-08-01 17:40:19 +0200

    Create palindrome function
    Created the palindrome function that passed all
    tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleaned = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replace(/ /g,'').toLowerCase()
+    let reversed = cleaned.split("").reverse().join("")
+    if(cleaned === reversed){
+        console.log(`${cleaned} => ${reversed} `)
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
