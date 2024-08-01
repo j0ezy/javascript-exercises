74360d3609eaa87e97c5cebb0fe2102b154d8cf5
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-08-01 17:56:43 +0200

    Create fibonacci function
    Created fibonacci function that also checks if the
    input is valid and passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num)
+    let currentValue = 1
+    let previousvalue = 0
 
+    if(num === 0) return 0
+    if(num < 0) return "OOPS"
+
+    for(let i = 1; i < num; i++){
+        let temp = currentValue
+        currentValue = previousvalue + currentValue
+        previousvalue = temp
+    }
+    return currentValue
 };
 
 // Do not edit below this line
