e286f6a4575ab04ce13681180eb3d5d3f52fb73a
j0ezy <145462961+j0ezy@users.noreply.github.com>
2025-06-29 21:30:06 +0200

    Fix varaible assignment

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -3,9 +3,9 @@ const sumAll = function(num1, num2) {
     if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR'
     if (num1 < 0 || num2 < 0) return 'ERROR'
 
-    min = num1 < num2 ? num1 : num2
-    max = num1 < num2 ? num2 : num1
-    sum = 0
+    let min = num1 < num2 ? num1 : num2
+    let max = num1 < num2 ? num2 : num1
+    let sum = 0
 
     for (let i = min; i <= max; i++) {
         sum += i
