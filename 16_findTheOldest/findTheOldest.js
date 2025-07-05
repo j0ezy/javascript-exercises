c788a5722b8ce6c789a331933b395ba44f310d5e
j0ezy <145462961+j0ezy@users.noreply.github.com>
2025-07-05 12:48:05 +0200

    Solve* 16 findTheOldest excercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a,b) => {
+        if(!(yearOfDeath in a)) {
+            a['yearOfDeath'] = new Date().getFullYear()
+        }
+        if(!(yearOfDeath in b)) {
+            b['yearOfDeath'] = new Date().getFullYear()
+        }
+        let firstAge = a.yearOfDeath - a.yearOfBirth
+        let secondAge = b.yearOfDeath - b.yearOfBirth
+        return (firstAge < secondAge) ? 1 : -1
+    })[0]
 };
 
 // Do not edit below this line
