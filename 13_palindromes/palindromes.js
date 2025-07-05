683d024c6fa2e4c8e1f93e18757a6ac0fdd0ecc1
j0ezy <145462961+j0ezy@users.noreply.github.com>
2025-07-05 11:41:09 +0200

    Solve 13 palindromes excercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let cleanedWord = word.split('').filter((value) => {
+        return /[a-zA-Z0-9]/.test(value)
+    }).join('').toLowerCase()
+
+    let reversedWord = cleanedWord.split('').reverse().join('')
+    
+    return (cleanedWord == reversedWord ? true : false)
 };
 
 // Do not edit below this line
