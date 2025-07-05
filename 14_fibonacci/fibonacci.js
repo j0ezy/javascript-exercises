40e3f97c5731ee150532dc4fd6ea0c8b904531ee
j0ezy <145462961+j0ezy@users.noreply.github.com>
2025-07-05 11:53:07 +0200

    Solve 14 fibonacci excercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if(+num == 0) return 0
+    if(+num < 0) return 'OOPS'
+
+    let first = 0
+    let second = 1
+
+    for (let i = 0; i < +num; i++) {
+        let temp = first + second
+        first = second
+        second = temp
+    }
+
+    return first
 };
 
 // Do not edit below this line
